bd616956f5d5ab67dfa1acd10738bdccc5cd4ad4
Aaron-Mackay <aaron.mackay@coop.co.uk>
2026-04-14 12:05:40 +0100

    Fix AiUsageLog migration backfill

--- prisma/migrations/20260414104851_migration_20260414104804/migration.sql
+++ prisma/migrations/20260414104851_migration_20260414104804/migration.sql
@@ -1,5 +1,12 @@
 -- AlterTable
-ALTER TABLE "AiUsageLog" ADD COLUMN     "inputTokens" INTEGER,
+ALTER TABLE "AiUsageLog"
+ADD COLUMN     "inputTokens" INTEGER,
 ADD COLUMN     "outputTokens" INTEGER,
-ADD COLUMN     "route" TEXT NOT NULL;
+ADD COLUMN     "route" TEXT;
 
+UPDATE "AiUsageLog"
+SET "route" = 'unknown'
+WHERE "route" IS NULL;
+
+ALTER TABLE "AiUsageLog"
+ALTER COLUMN "route" SET NOT NULL;
