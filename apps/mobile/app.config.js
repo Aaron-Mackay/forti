3740a11709ef09420e94b698045a4e493380be8e
Aaron-Mackay <aaron.mackay@coop.co.uk>
2026-05-18 11:05:25 +0100

    Fix expo updates

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,13 +1,17 @@
-const baseConfig = require('./app.json').expo;
-
-module.exports = () => {
+module.exports = ({config}) => {
   const apiBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL ?? process.env.NEXT_PUBLIC_APP_URL ?? null;
 
   return {
-    ...baseConfig,
+    ...config,
     extra: {
-      ...baseConfig.extra,
+      ...config.extra,
       apiBaseUrl,
     },
+    "plugins": [
+      "expo-font",
+      "expo-image",
+      "expo-secure-store",
+      "expo-web-browser"
+    ]
   };
 };
